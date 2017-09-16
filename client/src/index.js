f0d1108a2b706d9d2cf31c5e058edcd8e51b9992
Stephen Mayeux <stephenmayeux@gmail.com>
2017-09-16 10:34:46 -0500

    start react-redux set up

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+const logger = createLogger({ collapsed: true })
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+let middleware = [thunk]
+
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger)
+}
+
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
