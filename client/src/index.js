73567fed606767582352c519ce3b2b574c09aa6d
Stephen Mayeux <stephenmayeux@gmail.com>
2017-09-16 11:02:24 -0500

    merge vr-2

---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+const logger = createLogger({ collapsed: true })
+
+import App from './containers/App';
+import reducers from './reducers';
+
+let middleware = [thunk]
+
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger)
+}
+
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
---
+++ client/webpack.config.js
@@ -0,0 +1,41 @@
+var combineLoaders = require('webpack-combine-loaders');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: combineLoaders([{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          query: {
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]'
+          }
+        }])
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
+};
