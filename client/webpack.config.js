4dc98a7c0e9fd8fb402709159e03bbe2d67160be
Stephen Mayeux <stephenmayeux@gmail.com>
2017-09-16 10:51:51 -0500

    css modules and normalize

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,5 @@
+var combineLoaders = require('webpack-combine-loaders');
+
 module.exports = {
   entry: [
     './src/index.js'
@@ -9,12 +11,25 @@ module.exports = {
   },
   module: {
     loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: combineLoaders([{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          query: {
+            modules: true,
+            localIdentName: '[name]__[local]___[hash:base64:5]'
+          }
+        }])
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
