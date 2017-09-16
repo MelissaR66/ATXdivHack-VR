61b8e6124738fcf09021a8084b086a436e6888ea
Stephen Mayeux <stephenmayeux@gmail.com>
2017-09-16 16:06:18 -0500

    merge team/VR-3

--- client/src/components/VR/index.js
+++ client/src/components/VR/index.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react'
+import 'aframe';
+import { Entity, Scene } from 'aframe-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-export default class VR extends Component {
-  render() {
+export default class VR extends React.Component {
+  render () {
     return (
-      <h1>VR</h1>
-    )
+      <Scene>
+        <a-assets>
+          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
+          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
+        </a-assets>
+
+        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
+        <Entity primitive="a-light" type="ambient" color="#445451"/>
+        <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
+        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
+        <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+
+      </Scene>
+    );
   }
 }
