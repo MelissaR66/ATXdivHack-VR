6c3ec2d08ce3669237b1e069341c78ac81a1ae3a
Cerbes <Courtney@Courtneys-MacBook-Air.local>
2017-09-16 23:44:37 -0500

    Added desert skybox, floor, and black spider model

--- client/src/components/VR/index.js
+++ client/src/components/VR/index.js
@@ -10,9 +10,10 @@ export default class VR extends React.Component {
     return (
       <Scene>
         <a-assets>
-          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
-          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
-	        <a-asset-item id="spider" src={`${API_SERVER}/spider.dae`}></a-asset-item>
+          <img id="groundTexture" src="/src/assets/floor.jpg"/>
+          <img id="skyTexture" src="/src/assets/sky.jpg"/>
+	        <a-asset-item id="spider-obj" src="/src/assets/spider.obj"></a-asset-item>
+	        <a-asset-item id="spider-mtl" src="/src/assets/spider.mtl"></a-asset-item>
         </a-assets>
 
         <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
@@ -20,7 +21,7 @@ export default class VR extends React.Component {
         <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
         <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
         <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
-        <Entity primitive="a-collada-model" src="#spider" position={{x: 0, y: 2, z: -3}} scale={{ x: 12, y: 12, z: 12 }}/>
+        <Entity obj-model="obj: #spider-obj;" material="color: #000000" position={{x: 0, y: 0, z: -3}} scale={{ x: 20, y: 20, z: 20 }}/>
       </Scene>
     );
   }
